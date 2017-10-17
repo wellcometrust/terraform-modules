9be0da5d6eacb19f35f7d4fd66a95fd4a7aad310
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-10-17 13:56:11 +0100

    Add route 52 cross account hosted zone modukle

---
+++ route53_cross_account_hosted_zone/variables.tf
@@ -0,0 +1,11 @@
+variable "role_name" {
+  description = "Unique name of role to create"
+}
+
+variable "account_id" {
+  description = "ID of account to trust"
+}
+
+variable "hosted_zone_id" {
+  description = "ID of hosted zone to make available"
+}
