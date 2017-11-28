3243e2bf61439cbb9470e909bdb1b49f02e9a473
Alex Chan <a.chan@wellcome.ac.uk>
2017-11-28 10:29:17 +0000

    /s/locals/local

--- services/config/data.tf
+++ services/config/data.tf
@@ -1,10 +1,10 @@
 locals {
   default_path = "templates/${var.app_name}.ini.template"
-  actual_path  = "${var.config_template_path == "" ? var.config_template_path : locals.default_path}"
+  actual_path  = "${var.config_template_path == "" ? var.config_template_path : local.default_path}"
 }
 
 data "template_file" "config" {
-  template = "${file("${path.cwd}/${locals.actual_path}")}"
+  template = "${file("${path.cwd}/${local.actual_path}")}"
   vars     = "${var.template_vars}"
   count    = "${var.is_config_managed}"
 }
