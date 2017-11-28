7b5db2b28db2c91f77bb439ea527a6e06186bf49
Alex Chan <a.chan@wellcome.ac.uk>
2017-11-28 10:27:56 +0000

    Specify the default path in a different way

--- services/config/data.tf
+++ services/config/data.tf
@@ -1,5 +1,10 @@
+locals {
+  default_path = "templates/${var.app_name}.ini.template"
+  actual_path  = "${var.config_template_path == "" ? var.config_template_path : locals.default_path}"
+}
+
 data "template_file" "config" {
-  template = "${file("${path.cwd}/${var.config_template_path == "" ? var.config_template_path : templates/${var.app_name}.ini.template}")}"
+  template = "${file("${path.cwd}/${locals.actual_path}")}"
   vars     = "${var.template_vars}"
   count    = "${var.is_config_managed}"
 }
