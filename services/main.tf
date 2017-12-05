3ac1edce6deb752bbfc11e0473e0613bb7bb7637
Alex Chan <a.chan@wellcome.ac.uk>
2017-12-05 09:23:26 +0000

    Set a default healthcheck path based on path pattern

--- services/main.tf
+++ services/main.tf
@@ -1,3 +1,9 @@
+locals {
+  default_healthcheck_path  = "/management/healthcheck"
+  fallback_healthcheck_path = "${replace(var.path_pattern, "/*", local.default_healthcheck_path)}"
+  healthcheck_path          = "${var.healthcheck_path == "" ? local.fallback_healthcheck_path : var.healthcheck_path}"
+}
+
 module "service" {
   source              = "./ecs_service"
   service_name        = "${var.name}"
@@ -11,7 +17,7 @@ module "service" {
   path_pattern        = "${var.path_pattern}"
   alb_priority        = "${var.alb_priority}"
   desired_count       = "${var.desired_count}"
-  healthcheck_path    = "${var.healthcheck_path}"
+  healthcheck_path    = "${local.healthcheck_path}"
   infra_bucket        = "${var.infra_bucket}"
   host_name           = "${var.host_name}"
 
