e5456a5b4658f6dd79ba4c121ea9b81123e061a1
Robert Kenny <r.kenny@wellcome.ac.uk>
2017-12-06 11:28:57 +0000

    better names

--- service/iam.tf
+++ service/iam.tf
@@ -2,8 +2,8 @@ data "aws_iam_role" "task_role" {
   name = "${var.task_role_name}"
 }
 
-resource "aws_iam_role_policy" "s3_role" {
-  name   = "${var.name}_read_from_s3"
+resource "aws_iam_role_policy" "read_config_from_s3" {
+  name   = "${var.name}_read_config_from_s3"
   role   = "${data.aws_iam_role.task_role.arn}"
   policy = "${data.aws_iam_policy_document.allow_config_read.json}"
 }
