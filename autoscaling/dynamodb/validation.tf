b94fdd41690d42443a1f60e51a3d7497c3dd05ea
Alex Chan <alex@alexwlchan.net>
2018-01-18 12:00:41 +0000

    Make 'terraform fmt' happy

--- autoscaling/dynamodb/validation.tf
+++ autoscaling/dynamodb/validation.tf
@@ -8,30 +8,36 @@
 
 resource "null_resource" "check_read_target_utilization" {
   count = "${var.enable_read_scaling ? (var.read_target_utilization == "" ? 1 : 0) : 0}"
+
   "ERROR: If enable_read_scaling=true, you need to provide read_target_utilization" = true
 }
 
 resource "null_resource" "check_read_min_capacity" {
   count = "${var.enable_read_scaling ? (var.read_min_capacity == "" ? 1 : 0) : 0}"
+
   "ERROR: If enable_read_scaling=true, you need to provide read_min_capacity" = true
 }
 
 resource "null_resource" "check_read_max_capacity" {
   count = "${var.enable_read_scaling ? (var.read_max_capacity == "" ? 1 : 0) : 0}"
+
   "ERROR: If enable_read_scaling=true, you need to provide read_max_capacity" = true
 }
 
 resource "null_resource" "check_write_target_utilization" {
   count = "${var.enable_write_scaling ? (var.write_target_utilization == "" ? 1 : 0) : 0}"
+
   "ERROR: If enable_write_scaling=true, you need to provide write_target_utilization" = true
 }
 
 resource "null_resource" "check_write_min_capacity" {
   count = "${var.enable_write_scaling ? (var.write_min_capacity == "" ? 1 : 0) : 0}"
+
   "ERROR: If enable_write_scaling=true, you need to provide write_min_capacity" = true
 }
 
 resource "null_resource" "check_write_max_capacity" {
   count = "${var.enable_write_scaling ? (var.write_max_capacity == "" ? 1 : 0) : 0}"
+
   "ERROR: If enable_write_scaling=true, you need to provide write_max_capacity" = true
 }
