1b650bb686391417a85ad78293ba8318f8b6007c
Alex Chan <alex@alexwlchan.net>
2018-02-09 11:54:41 +0000

    Can we get away without this?

--- ecs/service/ecs_task/data.tf
+++ ecs/service/ecs_task/data.tf
@@ -72,7 +72,7 @@ data "aws_iam_role" "task_role" {
 # docs: https://www.terraform.io/docs/configuration/interpolation.html
 
 data "template_file" "name_val_pair" {
-  count    = "${var.config_vars_length + length(var.service_vars)}"
+  count    = "${len(var.config_vars) + length(var.service_vars)}"
   template = "{\"name\": $${jsonencode(key)}, \"value\": $${jsonencode(value)}}"
 
   vars {
