639b8635d5ff5ada199e66b11501324eab8d3a2d
Robert Kenny <kenoir@users.noreply.github.com>
2018-02-13 12:59:20 +0000

    Re-add ARN to SNS module. (#46)

--- sns/outputs.tf
+++ sns/outputs.tf
@@ -7,3 +7,8 @@ output "publish_policy" {
   description = "Policy that allows publishing to the created SNS topic"
   value       = "${data.aws_iam_policy_document.publish_to_topic.json}"
 }
+
+output "arn" {
+  description = "ARN of the created SNS topic"
+  value       = "${aws_sns_topic.topic.arn}"
+}
