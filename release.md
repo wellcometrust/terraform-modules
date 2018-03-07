e5cf6ab4a2a327d9eceacd6fa1bfa57314d77584
Kate Welling <k.welling@wellcome.ac.uk>
2018-03-07 13:43:04 +0000

    Change to allow a list of hosted zone roles within one role

---
+++ release.md
@@ -0,0 +1,3 @@
+REALEASE_TYPE: MAJOR
+
+This update allows you to specify multiple hosted zones within one hosted zone role.
--- route53_cross_account_hosted_zone/policy.tf
+++ route53_cross_account_hosted_zone/policy.tf
@@ -1,6 +1,7 @@
 resource "aws_iam_role_policy" "policy" {
   name = "${var.role_name}-route53-policy"
   role = "${aws_iam_role.role.id}"
+  count = "${var.hosted_zone_ids}"
 
   policy = <<EOF
 {
@@ -18,21 +19,21 @@ resource "aws_iam_role_policy" "policy" {
          "Action": [
             "route53:GetHostedZone"
          ],
-         "Resource": "arn:aws:route53:::hostedzone/${var.hosted_zone_id}"
+         "Resource": "arn:aws:route53:::hostedzone/${element(var.hosted_zone_ids, count.index)}"
       },
       {
          "Effect": "Allow",
          "Action": [
             "route53:ListResourceRecordSets"
          ],
-         "Resource": "arn:aws:route53:::hostedzone/${var.hosted_zone_id}"
+         "Resource": "arn:aws:route53:::hostedzone/${element(var.hosted_zone_ids, count.index)}"
       },
       {
          "Effect": "Allow",
          "Action": [
             "route53:ChangeResourceRecordSets"
          ],
-         "Resource": "arn:aws:route53:::hostedzone/${var.hosted_zone_id}"
+         "Resource": "arn:aws:route53:::hostedzone/${element(var.hosted_zone_ids, count.index)}"
       }
    ]
 }
