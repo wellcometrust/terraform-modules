b45e37b988f7abba9e06edf2e64c194ae4746607
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-04-30 15:36:51 +0100

    Add name tag to Autoscaling Group module

--- ec2/asg/cloudformation/asg.tf
+++ ec2/asg/cloudformation/asg.tf
@@ -16,5 +16,6 @@ data "template_file" "cluster_ecs_asg" {
     asg_min_size        = "${var.asg_min}"
     asg_desired_size    = "${var.asg_desired}"
     asg_max_size        = "${var.asg_max}"
+    asg_name            = "${var.asg_name}"
   }
 }
