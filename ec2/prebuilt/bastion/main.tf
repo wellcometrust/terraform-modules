bf31c7e2d73ddbe18c9325ca8a244efef71c488f
Alex Chan <a.chan@wellcome.ac.uk>
2018-06-25 14:57:48 +0100

    Rearrange variables to match the corresponding variables.tf

--- ec2/prebuilt/bastion/main.tf
+++ ec2/prebuilt/bastion/main.tf
@@ -1,26 +1,25 @@
 module "cloudformation_stack" {
   source = "../../modules/asg"
 
-  subnet_list        = "${var.subnet_list}"
-  asg_name           = "${var.name}"
-  launch_config_name = "${module.launch_config.name}"
+  asg_name = "${var.name}"
 
   asg_max     = "${var.asg_max}"
   asg_desired = "${var.asg_desired}"
   asg_min     = "${var.asg_min}"
+
+  subnet_list        = "${var.subnet_list}"
+  launch_config_name = "${module.launch_config.name}"
 }
 
 module "launch_config" {
   source = "../../modules/launch_config/ondemand"
 
+  key_name              = "${var.key_name}"
+  image_id              = "${var.image_id}"
+  instance_type         = "${var.instance_type}"
   instance_profile_name = "${module.instance_profile.name}"
+  user_data             = "${var.user_data}"
 
-  vpc_id                      = "${var.vpc_id}"
-  asg_name                    = "${var.name}"
-  image_id                    = "${var.image_id}"
-  user_data                   = "${var.user_data}"
-  key_name                    = "${var.key_name}"
-  instance_type               = "${var.instance_type}"
   associate_public_ip_address = "${var.public_ip}"
 
   instance_security_groups = ["${module.security_groups.instance_security_groups}"]
@@ -29,12 +28,13 @@ module "launch_config" {
 module "security_groups" {
   source = "../../modules/security_groups"
 
+  custom_security_groups = ["${var.custom_security_groups}"]
+
   name   = "${var.name}"
   vpc_id = "${var.vpc_id}"
 
   controlled_access_cidr_ingress    = ["${var.controlled_access_cidr_ingress}"]
   controlled_access_security_groups = ["${var.ssh_ingress_security_groups}"]
-  custom_security_groups            = ["${var.custom_security_groups}"]
 }
 
 module "instance_profile" {
