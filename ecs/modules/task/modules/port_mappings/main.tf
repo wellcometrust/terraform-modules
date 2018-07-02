ec07db08e42410f5a7bae19c111d664ae3415f65
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-07-02 17:37:48 +0100

    should be empty string

--- ecs/modules/task/modules/port_mappings/main.tf
+++ ecs/modules/task/modules/port_mappings/main.tf
@@ -34,7 +34,7 @@ locals {
   port_mapping_without_host_string = "[${replace(jsonencode(local.port_mapping_without_host), "/\"([0-9]+\\.?[0-9]*)\"/", "$1")}]"
   port_mapping_string_without_check = "${var.expose_port == "false" ? local.port_mapping_without_host_string : local.port_mapping_with_host_string}"
 
-  port_mapping_string = "${var.container_port == "false" ? "{}" : local.port_mapping_string_without_check}"
+  port_mapping_string = "${var.container_port == "false" ? "[]" : local.port_mapping_string_without_check}"
 }
 
 output "port_mappings_string" {
