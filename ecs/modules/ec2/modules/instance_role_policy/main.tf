86cab372316a9d9b18573dbe760fb803bddad5bf
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-07-03 13:15:21 +0100

    remove unnecessary networkmode awsvpc in container defintion

--- ecs/modules/ec2/modules/instance_role_policy/main.tf
+++ ecs/modules/ec2/modules/instance_role_policy/main.tf
@@ -19,7 +19,7 @@ data "aws_iam_policy_document" "instance_policy" {
 }
 
 resource "aws_iam_role_policy" "instance" {
-  name   = "${var.cluster_name}_instance_role_policy"
+  name = "${var.cluster_name}_instance_role_policy"
 
   role   = "${var.instance_profile_role_name}"
   policy = "${data.aws_iam_policy_document.instance_policy.json}"
