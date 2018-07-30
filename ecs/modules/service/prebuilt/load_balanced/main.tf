0b474896b25c16d3edd3b5c141e4a9ed7e723a18
alicefuzier <alicefuzier@gmail.com>
2018-07-30 16:05:02 +0100

    Adding alarms for alb errors

--- ecs/modules/service/prebuilt/load_balanced/main.tf
+++ ecs/modules/service/prebuilt/load_balanced/main.tf
@@ -60,3 +60,26 @@ resource "aws_alb_target_group" "ecs_service" {
     matcher  = "200"
   }
 }
+
+module "alb_alarm" {
+  source = "alb_alarm"
+  # The rule here is: if we have less than the minimum percent of
+  # healthy hosts, we should alarm.
+  #
+  # If this is zero (i.e. it's okay to go down to no running hosts), we don't
+  # alarm --- this is not an uptime-critical service.
+  enable_alarm = "${ceil(local.healthy_host_threshold) > 0 && var.enable_alb_alarm ? 1 : 0}"
+  tg_dimension = "${aws_alb_target_group.ecs_service.arn_suffix}"
+  lb_dimension = "${var.loadbalancer_cloudwatch_id}"
+  name   = "${var.service_name}-alb-not-enough-healthy-hosts"
+  comparison_operator = "LessThanThreshold"
+  metric              = "HealthyHostCount"
+  topic_arn           = "${var.server_error_alarm_topic_arn}"
+
+  threshold = "${local.healthy_host_threshold}"
+
+  # There have been scenarios where HealthyHostCount doesn't report any data
+  # (e.g. when an invalid container definition was pushed).  In this case,
+  # we still want an alarm!  So if we don't have data, assume something is up.
+  treat_missing_data = "breaching"
+}
