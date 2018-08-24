de3514da44db093775652ed417109cb4234c9eee
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2018-08-24 13:56:36 +0000

    Apply auto-formatting rules

--- sqs/subscription.tf
+++ sqs/subscription.tf
@@ -10,6 +10,6 @@ locals {
 }
 
 resource "null_resource" "topic_count_check" {
-  count = "${local.incorrect_topic_count}"
+  count                                                                                = "${local.incorrect_topic_count}"
   "ERROR: You need to update the value of the topic_count variable in your SNS module" = true
 }
