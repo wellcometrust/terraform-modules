32852ae1a29e063ed20c395e93cb3f3a7673b188
alicefuzier <alicefuzier@gmail.com>
2018-10-26 16:12:25 +0100

    Output using *

--- ecs/modules/service/prebuilt/load_balanced/outputs.tf
+++ ecs/modules/service/prebuilt/load_balanced/outputs.tf
@@ -3,9 +3,9 @@ output "service_name" {
 }
 
 output "target_group_arn" {
-  value = "${var.target_group_protocol == "HTTP"? aws_alb_target_group.http_target_group.arn : aws_alb_target_group.tcp_target_group.arn}"
+  value = "${var.target_group_protocol == "HTTP"? aws_alb_target_group.http_target_group.*.arn : aws_alb_target_group.tcp_target_group.*.arn}"
 }
 
 output "target_group_arn_suffix" {
-  value = "${var.target_group_protocol == "HTTP"? aws_alb_target_group.http_target_group.arn_suffix : aws_alb_target_group.tcp_target_group.arn_suffix}"
+  value = "${var.target_group_protocol == "HTTP"? aws_alb_target_group.http_target_group.*.arn_suffix : aws_alb_target_group.tcp_target_group.*.arn_suffix}"
 }
