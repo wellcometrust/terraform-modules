7474a365aaf8f3d701aa3d362104f95808cc01d3
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-11-06 11:06:07 +0000

    fmt

--- api_gateway/modules/resource/no_auth/locals.tf
+++ api_gateway/modules/resource/no_auth/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  domain     = "${var.proxied_hostname}:${var.forward_port}"
-  uri        = "http://${local.domain}/${var.forward_path}"
+  domain = "${var.proxied_hostname}:${var.forward_port}"
+  uri    = "http://${local.domain}/${var.forward_path}"
 }
