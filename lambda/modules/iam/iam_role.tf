60d0c706b3e9d3ff0dfcda9d17c7e0ba07834278
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-11-15 17:48:23 +0000

    lambda fixes

--- lambda/modules/iam/iam_role.tf
+++ lambda/modules/iam/iam_role.tf
@@ -1,3 +1,4 @@
 resource "aws_iam_role" "iam_role" {
+  name = "${var.name}"
   assume_role_policy = "${data.aws_iam_policy_document.assume_lambda_role.json}"
 }
--- lambda/modules/monitoring/iam_role_policy.tf
+++ lambda/modules/monitoring/iam_role_policy.tf
@@ -3,3 +3,9 @@ resource "aws_iam_role_policy" "lambda_dlq" {
   role   = "${var.iam_role_name}"
   policy = "${data.aws_iam_policy_document.lambda_dlq.json}"
 }
+
+resource "aws_iam_role_policy" "cloudwatch_logs" {
+  name   = "${var.iam_role_name}_lambda_dlq"
+  role   = "${var.iam_role_name}"
+  policy = "${data.aws_iam_policy_document.cloudwatch_logs.json}"
+}
