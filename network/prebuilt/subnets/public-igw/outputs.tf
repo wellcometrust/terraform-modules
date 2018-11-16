03102d7e14e50f663a0cbec0026f7edde1519022
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-11-16 13:39:19 +0000

    OUtput route table ids

--- network/prebuilt/subnets/public-igw/outputs.tf
+++ network/prebuilt/subnets/public-igw/outputs.tf
@@ -2,6 +2,10 @@ output "subnets" {
   value = ["${module.subnets.subnets}"]
 }
 
+output "route_table_id" {
+  value = "${module.subnets.route_table_id}"
+}
+
 output "az_count" {
   value = "${var.az_count}"
 }
--- network/prebuilt/vpc/public-private-igw/outputs.tf
+++ network/prebuilt/vpc/public-private-igw/outputs.tf
@@ -6,6 +6,14 @@ output "public_subnets" {
   value = ["${module.public_subnets.subnets}"]
 }
 
+output "public_route_table_id" {
+  value = "${module.public_subnets.route_table_id}"
+}
+
+output "private_route_table_id" {
+  value = "${module.private_subnets.route_table_id}"
+}
+
 output "vpc_id" {
   value = "${aws_vpc.vpc.id}"
 }
