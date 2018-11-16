7ad8f0c2a4fcf0b540ebcf4cd51cb2b84af96cd5
alicefuzier <alicefuzier@gmail.com>
2018-11-16 12:47:30 +0000

    Proper name for policy

--- lambda/modules/monitoring/iam_role_policy.tf
+++ lambda/modules/monitoring/iam_role_policy.tf
@@ -5,7 +5,7 @@ resource "aws_iam_role_policy" "lambda_dlq" {
 }
 
 resource "aws_iam_role_policy" "cloudwatch_logs" {
-  name   = "${var.iam_role_name}_lambda_dlq"
+  name   = "${var.iam_role_name}_lambda_logs"
   role   = "${var.iam_role_name}"
   policy = "${data.aws_iam_policy_document.cloudwatch_logs.json}"
 }
