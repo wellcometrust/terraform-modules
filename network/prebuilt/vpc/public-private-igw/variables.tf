4ced825a55f3084d821d4317249746a6b371478a
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-11-23 14:00:57 +0000

    Set default for prebuilt VPC module enable_dns_hostnames to true
    In order that service discovery works as expected

--- network/prebuilt/vpc/public-private-igw/variables.tf
+++ network/prebuilt/vpc/public-private-igw/variables.tf
@@ -8,10 +8,12 @@ variable "cidr_block_private" {}
 variable "cidrsubnet_newbits_public" {}
 variable "cidrsubnet_newbits_private" {}
 
-variable "public_az_count" {
-  default = ""
-}
+variable "public_az_count" {}
+variable "private_az_count" {}
 
-variable "private_az_count" {
-  default = ""
+variable "enable_dns_support" {
+  default = true
+}
+variable "enable_dns_hostnames" {
+  default = true
 }
