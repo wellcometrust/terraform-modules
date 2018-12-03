34863343ce084003a7b6cb36de6dc2d946e57a56
alicefuzier <alicefuzier@gmail.com>
2018-12-03 15:52:24 +0000

    Allow caching per stage

--- api_gateway/modules/stage/main.tf
+++ api_gateway/modules/stage/main.tf
@@ -9,6 +9,18 @@ resource "aws_api_gateway_stage" "stage" {
   stage_name    = "${var.stage_name}"
   rest_api_id   = "${var.api_id}"
   deployment_id = "${aws_api_gateway_deployment.stage.id}"
-
+  cache_cluster_enabled = "${var.cache_enabled}"
+  cache_cluster_size = "${var.cache_size}"
   variables = "${var.variables}"
+}
+
+resource "aws_api_gateway_method_settings" "s" {
+  rest_api_id = "${var.api_id}"
+  stage_name  = "${var.stage_name}"
+  method_path = "*/*"
+
+  settings {
+    caching_enabled = "${var.cache_enabled}"
+  cache_ttl_in_seconds = "${var.cache_ttl_in_seconds}"
+  }
 }
--- api_gateway/modules/stage/variables.tf
+++ api_gateway/modules/stage/variables.tf
@@ -10,3 +10,15 @@ variable "depends_on" {
 }
 
 variable "stage_name" {}
+
+variable "cache_enabled" {
+  default = "false"
+}
+
+variable "cache_size" {
+  default = "1.6"
+}
+
+variable "cache_ttl_in_seconds" {
+  default = "60"
+}
