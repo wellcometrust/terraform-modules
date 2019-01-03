8c6d9a2b036c7e6f7489130aad4179336bb38fcb
alicefuzier <alicefuzier@gmail.com>
2019-01-03 15:25:46 +0000

    Don't blat stage variables with new deployment

--- api_gateway/modules/stage/main.tf
+++ api_gateway/modules/stage/main.tf
@@ -2,9 +2,7 @@ resource "aws_api_gateway_deployment" "stage" {
   rest_api_id = "${var.api_id}"
   stage_name  = "${var.stage_name}"
 
-  variables {
-    dependencies_md5 = "${md5(join(",", var.depends_on))}"
-  }
+  variables = "${merge(var.variables, map("dependencies_md5", md5(join(",", var.depends_on))))}"
 }
 
 resource "aws_api_gateway_stage" "stage" {
@@ -12,5 +10,5 @@ resource "aws_api_gateway_stage" "stage" {
   rest_api_id   = "${var.api_id}"
   deployment_id = "${aws_api_gateway_deployment.stage.id}"
 
-  variables = "${var.variables}"
+  variables = "${merge(var.variables, map("dependencies_md5", md5(join(",", var.depends_on))))}"
 }
