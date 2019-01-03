0935ffac00e216c873a1c8343371c322617a91f7
alicefuzier <alicefuzier@gmail.com>
2019-01-03 16:38:19 +0000

    Comment

--- api_gateway/modules/stage/main.tf
+++ api_gateway/modules/stage/main.tf
@@ -1,8 +1,15 @@
+locals {
+  // Add hash of dependencies to the variables block to force
+  // new deployment if dependencies change
+  variables = "${merge(var.variables, map("dependencies_md5", md5(join(",", var.depends_on))))}"
+}
+
 resource "aws_api_gateway_deployment" "stage" {
   rest_api_id = "${var.api_id}"
   stage_name  = "${var.stage_name}"
 
-  variables = "${merge(var.variables, map("dependencies_md5", md5(join(",", var.depends_on))))}"
+  variables = "${local.variables}"
+
   lifecycle {
     create_before_destroy = true
   }
@@ -13,7 +20,5 @@ resource "aws_api_gateway_stage" "stage" {
   rest_api_id   = "${var.api_id}"
   deployment_id = "${aws_api_gateway_deployment.stage.id}"
 
-  variables = "${merge(var.variables, map("dependencies_md5", md5(join(",", var.depends_on))))}"
-
-
+  variables = "${local.variables}"
 }
