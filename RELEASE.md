8781bad4d6b80a1fd9469aed14bab484ee0407a4
Robert Kenny <kenoir@users.noreply.github.com>
2019-01-03 14:10:14 +0000

    Adds some additional outputs for the VHS module (#132)

---
+++ RELEASE.md
@@ -0,0 +1,3 @@
+RELEASE_TYPE: patch 
+
+Adds some addition outputs to the VHS module for the contained dynamo table.
--- vhs/modules/vhs/outputs.tf
+++ vhs/modules/vhs/outputs.tf
@@ -13,3 +13,15 @@ output "read_policy" {
 output "full_access_policy" {
   value = "${data.aws_iam_policy_document.full_access_policy.json}"
 }
+
+output "table_name" {
+  value = "${module.dynamo.table_name}"
+}
+
+output "table_arn" {
+  value = "${module.dynamo.table_arn}"
+}
+
+output "table_stream_arn" {
+  value = "${module.dynamo.table_stream_arn}"
+}
