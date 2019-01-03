0fc181f5db20a03b6dc417d0ff61f51d616e1861
alicefuzier <alicefuzier@gmail.com>
2019-01-03 12:04:30 +0000

    Try using method response

--- api_gateway/prebuilt/method/static/outputs.tf
+++ api_gateway/prebuilt/method/static/outputs.tf
@@ -5,6 +5,6 @@ output "http_method" {
 output "integration_id" {
   value = "${module.static_integration.id}"
 }
-output "integration_uri" {
-  value = "${module.static_integration.uri}"
+output "method_response_resource_id" {
+  value = "${aws_api_gateway_method_response.200.resource_id}"
 }
