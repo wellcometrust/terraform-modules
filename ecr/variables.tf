f3c2210b75eeaf0382f6c4861b1e5a649fae411c
Robert Kenny <kenoir@users.noreply.github.com>
2019-01-14 12:28:00 +0000

    Protect ECR optionally (#136)
    * Optionally (and by default) protect ECR repos
    
    * add release.md
    
    * fmt

--- ecr/variables.tf
+++ ecr/variables.tf
@@ -5,3 +5,8 @@ variable "id" {
 variable "namespace" {
   description = "Namespace prefix to ECR repository"
 }
+
+variable "protected" {
+  description = "Protect resource from deletion"
+  default     = true
+}
