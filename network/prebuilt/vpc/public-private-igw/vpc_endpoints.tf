67eeffff390edd8978b2cbc581e2a2c117bb23e3
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2019-01-16 13:06:56 +0000

    Apply auto-formatting rules

--- network/prebuilt/vpc/public-private-igw/vpc_endpoints.tf
+++ network/prebuilt/vpc/public-private-igw/vpc_endpoints.tf
@@ -1,15 +1,15 @@
 module "dynamodb_endpoint" {
   source = "./gateway_endpoint"
 
-  service = "dynamodb"
-  vpc_id  = "${aws_vpc.vpc.id}"
+  service        = "dynamodb"
+  vpc_id         = "${aws_vpc.vpc.id}"
   route_table_id = "${module.private_subnets.route_table_id}"
 }
 
 module "s3_endpoint" {
   source = "./gateway_endpoint"
 
-  service = "s3"
-  vpc_id  = "${aws_vpc.vpc.id}"
+  service        = "s3"
+  vpc_id         = "${aws_vpc.vpc.id}"
   route_table_id = "${module.private_subnets.route_table_id}"
 }
