ca72fac9abbf0d9d258dc57347c548aa4a350890
Robert Kenny <r.kenny@wellcome.ac.uk>
2019-01-21 15:46:27 +0000

    fmt

--- iam/prebuilt/users/console_user/main.tf
+++ iam/prebuilt/users/console_user/main.tf
@@ -11,3 +11,15 @@ resource "aws_iam_access_key" "access_key" {
   user    = "${aws_iam_user.user.name}"
   pgp_key = "${var.pgp_key}"
 }
+
+resource "aws_iam_user_policy" "policy" {
+  user   = "${aws_iam_user.user.name}"
+  policy = "${data.aws_iam_policy_document.sts.json}"
+}
+
+data "aws_iam_policy_document" "sts" {
+  "statement" {
+    actions   = ["sts:*"]
+    resources = ["*"]
+  }
+}
