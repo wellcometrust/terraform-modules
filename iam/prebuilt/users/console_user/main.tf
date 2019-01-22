8d4e78a5fd860253ba482751d451c3e37a52a183
Robert Kenny <r.kenny@wellcome.ac.uk>
2019-01-22 10:35:38 +0000

    allow new users to sign-in and change passwd

--- iam/prebuilt/users/console_user/main.tf
+++ iam/prebuilt/users/console_user/main.tf
@@ -137,6 +137,8 @@ data "aws_iam_policy_document" "mfa" {
       "iam:ListMFADevices",
       "iam:GetAccountSummary",
       "sts:GetSessionToken",
+      "iam:ChangePassword",
+      "iam:CreateLoginProfile"
     ]
 
     resources = ["*"]
