8d3e53176613bc601fb8a0b503046537306f540d
Robert Kenny <r.kenny@wellcome.ac.uk>
2019-01-22 10:30:10 +0000

    Enforce MFA

--- iam/prebuilt/users/console_user/main.tf
+++ iam/prebuilt/users/console_user/main.tf
@@ -12,14 +12,139 @@ resource "aws_iam_access_key" "access_key" {
   pgp_key = "${var.pgp_key}"
 }
 
-resource "aws_iam_user_policy" "policy" {
+resource "aws_iam_user_policy" "sts" {
   user   = "${aws_iam_user.user.name}"
   policy = "${data.aws_iam_policy_document.sts.json}"
+}
+
+resource "aws_iam_user_policy" "mfa" {
+  user   = "${aws_iam_user.user.name}"
+  policy = "${data.aws_iam_policy_document.mfa.json}"
 }
 
 data "aws_iam_policy_document" "sts" {
   "statement" {
     actions   = ["sts:*"]
     resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "mfa" {
+  "statement" {
+    effect = "Allow"
+
+    actions = [
+      "iam:ListAccountAliases",
+      "iam:ListUsers",
+      "iam:ListVirtualMFADevices",
+      "iam:GetAccountPasswordPolicy",
+      "iam:GetAccountSummary",
+    ]
+
+    resources = ["*"]
+  }
+
+  "statement" {
+    effect = "Allow"
+
+    actions = [
+      "iam:ChangePassword",
+      "iam:CreateAccessKey",
+      "iam:CreateLoginProfile",
+      "iam:DeleteAccessKey",
+      "iam:DeleteLoginProfile",
+      "iam:GetLoginProfile",
+      "iam:ListAccessKeys",
+      "iam:UpdateAccessKey",
+      "iam:UpdateLoginProfile",
+      "iam:ListSigningCertificates",
+      "iam:DeleteSigningCertificate",
+      "iam:UpdateSigningCertificate",
+      "iam:UploadSigningCertificate",
+      "iam:ListSSHPublicKeys",
+      "iam:GetSSHPublicKey",
+      "iam:DeleteSSHPublicKey",
+      "iam:UpdateSSHPublicKey",
+      "iam:UploadSSHPublicKey",
+    ]
+
+    resources = [
+      "arn:aws:iam::*:user/${aws_iam_user.user.name}",
+    ]
+  }
+
+  "statement" {
+    effect = "Allow"
+
+    actions = [
+      "iam:ListMFADevices",
+    ]
+
+    resources = [
+      "arn:aws:iam::*:mfa/*",
+      "arn:aws:iam::*:user/${aws_iam_user.user.name}",
+    ]
+  }
+
+  "statement" {
+    effect = "Allow"
+
+    actions = [
+      "iam:CreateVirtualMFADevice",
+      "iam:DeleteVirtualMFADevice",
+      "iam:EnableMFADevice",
+      "iam:ResyncMFADevice",
+    ]
+
+    resources = [
+      "arn:aws:iam::*:mfa/${aws_iam_user.user.name}",
+      "arn:aws:iam::*:user/${aws_iam_user.user.name}",
+    ]
+  }
+
+  "statement" {
+    effect = "Allow"
+
+    actions = [
+      "iam:DeactivateMFADevice",
+    ]
+
+    resources = [
+      "arn:aws:iam::*:mfa/${aws_iam_user.user.name}",
+      "arn:aws:iam::*:user/${aws_iam_user.user.name}",
+    ]
+
+    condition {
+      test     = "Bool"
+      values   = [true]
+      variable = "aws:MultiFactorAuthPresent"
+    }
+  }
+
+  "statement" {
+    effect = "Deny"
+
+    not_actions = [
+      "iam:CreateVirtualMFADevice",
+      "iam:ListVirtualMFADevices",
+      "iam:EnableMFADevice",
+      "iam:ResyncMFADevice",
+      "iam:ListAccountAliases",
+      "iam:ListUsers",
+      "iam:ListSSHPublicKeys",
+      "iam:ListAccessKeys",
+      "iam:ListServiceSpecificCredentials",
+      "iam:ListMFADevices",
+      "iam:GetAccountSummary",
+      "sts:GetSessionToken",
+    ]
+
+    resources = ["*"]
+
+    condition {
+      test     = "Bool"
+      values   = [false]
+      variable = "aws:MultiFactorAuthPresent"
+    }
   }
 }
