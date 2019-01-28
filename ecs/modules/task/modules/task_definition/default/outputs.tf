fab0d2ca2d75046980a84f62ce3d56c43e7dd6f3
Alex Chan <alex@alexwlchan.net>
2019-01-28 17:22:36 +0000

    Make sure we expose the task role ARNs

--- ecs/modules/task/modules/task_definition/default/outputs.tf
+++ ecs/modules/task/modules/task_definition/default/outputs.tf
@@ -2,6 +2,14 @@ output "task_definition_arn" {
   value = "${aws_ecs_task_definition.task.arn}"
 }
 
+output "task_role_arn" {
+  value = "${module.iam_roles.task_role_arn}"
+}
+
+output "task_execution_role_arn" {
+  value = "${module.iam_roles.task_execution_role_arn}"
+}
+
 output "task_role_name" {
   value = "${module.iam_roles.name}"
 }
