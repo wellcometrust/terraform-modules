6aca924ea94f559484160523506c3698eead7730
James A Gorrie <j.gorrie@wellcome.ac.uk>
2019-05-24 09:46:52 +0100

    add cross account access to SNS topics

---
+++ RELEASE.md
@@ -0,0 +1,3 @@
+RELEASE_TYPE: patch
+
+Add cross account access permissions to be set for SNS topics
--- sns/variables.tf
+++ sns/variables.tf
@@ -1,3 +1,9 @@
 variable "name" {
   description = "Name of the SNS topic"
 }
+
+variable "cross_account_subscription_ids" {
+  type        = "list"
+  default     = []
+  description = "AWS account IDs alolowed to subscribe to this topic"
+}
