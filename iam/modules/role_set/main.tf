18b610b7b29a1925f3ba0a15ed86da56529641db
Robert Kenny <r.kenny@wellcome.ac.uk>
2019-06-28 17:40:34 +0100

    fix path

--- iam/modules/role_set/main.tf
+++ iam/modules/role_set/main.tf
@@ -7,7 +7,7 @@ module "role" {
 }
 
 module "role_policy" {
-  source = "../../role_policies/assume_role"
+  source = "../../prebuilt/role_policies/assume_role"
   role_name = "${module.role.name}"
 
   assumable_roles = "${var.assumable_role_arns}"
