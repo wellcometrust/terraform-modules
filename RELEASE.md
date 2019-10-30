d26bee17e8799896576ac32375a485a45ee4cbf9
Alex Chan <alex@alexwlchan.net>
2019-10-30 09:42:47 +0000

    Export role names from the AWS account module

---
+++ RELEASE.md
@@ -0,0 +1,5 @@
+RELEASE_TYPE: minor
+
+In `iam/prebuilt/account/aws`, export the IAM role names as well as the ARNs.
+
+Additionally, if you want to get all the role ARNs or names, you can use `all_role_arns` or `all_role_names`.
--- iam/prebuilt/account/aws/outputs.tf
+++ iam/prebuilt/account/aws/outputs.tf
@@ -21,3 +21,49 @@ output "read_only_role_arn" {
 output "publisher_role_arn" {
   value = "${module.publisher_role.arn}"
 }
+
+output "all_role_arns" {
+  value = [
+    "${module.admin_role.arn}",
+    "${module.billing_role.arn}",
+    "${module.developer_role.arn}",
+    "${module.monitoring_role.arn}",
+    "${module.read_only_role.arn}",
+    "${module.publisher_role.arn}",
+  ]
+}
+
+output "admin_role_name" {
+  value = "${module.admin_role.name}"
+}
+
+output "billing_role_name" {
+  value = "${module.billing_role.name}"
+}
+
+output "developer_role_name" {
+  value = "${module.developer_role.name}"
+}
+
+output "monitoring_role_name" {
+  value = "${module.monitoring_role.name}"
+}
+
+output "read_only_role_name" {
+  value = "${module.read_only_role.name}"
+}
+
+output "publisher_role_name" {
+  value = "${module.publisher_role.name}"
+}
+
+output "all_role_names" {
+  value = [
+    "${module.admin_role.name}",
+    "${module.billing_role.name}",
+    "${module.developer_role.name}",
+    "${module.monitoring_role.name}",
+    "${module.read_only_role.name}",
+    "${module.publisher_role.name}",
+  ]
+}
