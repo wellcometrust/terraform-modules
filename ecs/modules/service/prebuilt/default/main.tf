767321864a93e1938c14c71cb6a761f3c80fb68f
Alex Chan <alex@alexwlchan.net>
2019-11-07 10:21:25 +0000

    y so many modules

--- ecs/modules/service/prebuilt/default/main.tf
+++ ecs/modules/service/prebuilt/default/main.tf
@@ -15,9 +15,9 @@ resource "aws_ecs_service" "service" {
 
   launch_type = "${var.launch_type}"
 
-  network_configuration = {
-    subnets          = ["${var.subnets}"]
-    security_groups  = ["${var.security_group_ids}"]
+  network_configuration {
+    subnets          = "${var.subnets}"
+    security_groups  = "${var.security_group_ids}"
     assign_public_ip = false
   }
 
