0ee9e333d66262287af07ab21f9d3eb8f954c3be
alicefuzier <alicefuzier@gmail.com>
2019-12-16 13:50:38 +0000

    remove topic_count check

--- sqs/subscription.tf
+++ sqs/subscription.tf
@@ -4,12 +4,3 @@ resource "aws_sns_topic_subscription" "sns_topic" {
   topic_arn = "${element(var.topic_arns, count.index)}"
   endpoint  = "${aws_sqs_queue.q.arn}"
 }
-
-locals {
-  incorrect_topic_count = "${length(var.topic_arns) == var.topic_count ? 0 : 1}"
-}
-
-resource "null_resource" "topic_count_check" {
-  count                                                                                = "${local.incorrect_topic_count}"
-  "ERROR: You need to update the value of the topic_count variable in your SNS module" = true
-}
