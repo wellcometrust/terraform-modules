95ccf5575e70cb29689ef17484d0683e2098e443
alicefuzier <alicefuzier@gmail.com>
2019-12-16 13:23:34 +0000

    Remive unused variables

--- sqs/variables.tf
+++ sqs/variables.tf
@@ -36,14 +36,6 @@ variable "receive_wait_time_seconds" {
   default     = 0
 }
 
-variable "aws_region" {
-  description = "AWS region to create queue in"
-}
-
-variable "account_id" {
-  description = "AWS account id for account to create queue in"
-}
-
 variable "max_receive_count" {
   description = "Max receive count before sending to DLQ"
   default     = "4"
